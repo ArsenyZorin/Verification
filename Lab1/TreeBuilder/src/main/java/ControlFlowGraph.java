87fe47e43df6e3ffa22ebd191cc54a889fbaacda
Arseny Zorin <zorinarseny@yandex.ru>
2017-10-24 17:35:58 +0300

    Recursive splitJumped method

--- Lab1/TreeBuilder/src/main/java/ControlFlowGraph.java
+++ Lab1/TreeBuilder/src/main/java/ControlFlowGraph.java
@@ -32,13 +32,49 @@ public class ControlFlowGraph{
                         node.nodeName.contains("LBRACE") ||
                         node.nodeName.contains("RBRACE"))
                     continue;
-                if (!isJumped(node))
+                if (!isJumped(node)) {
                     nodeShapes.add(new GraphElement(node, ElementShape.SQUARE));
-                else
-                    nodeShapes.add(new GraphElement(node, ElementShape.DIAMOND));
+                }
+                else {
+                    if("BINARY_EXPRESSION".equals(node.nodeName)) {
+                        nodeShapes.add(new GraphElement(node, ElementShape.DIAMOND));
+                        continue;
+                    }
+                    nodeShapes.addAll(splitJumped(node));
+                }
             }
             treeShapes.add(nodeShapes);
         }
+    }
+
+    private List<GraphElement> splitJumped(ASTEntry jumpedNode){
+        List<GraphElement> nodeList = new ArrayList<>();
+        for(ASTEntry node : jumpedNode.children) {
+            if ("BLOCK_STATEMENT".equals(node.nodeName) ||
+                    "CODE_STATEMENT".equals(node.nodeName))
+                nodeList.addAll(splitJumped(node));
+
+            if (!isJumped(node))
+                nodeList.add(new GraphElement(node, ElementShape.SQUARE));
+            else {
+                if("BINARY_EXPRESSION".equals(node.nodeName)){
+                    nodeList.add(new GraphElement(node, ElementShape.DIAMOND));
+                    continue;
+                }
+                nodeList.addAll(splitJumped(node));
+            }
+        }
+/*
+        if("FOR_STATEMENT".equals(jumpedNode.nodeName))
+            for (ASTEntry node : jumpedNode.children) {
+                if ("DECLARATION_STATEMENT".equals(node.nodeName))
+                    nodeList.add(new GraphElement(node, ElementShape.SQUARE));
+                else if("BINARY_EXPRESSION".equals(node.nodeName))
+                    nodeList.add(new GraphElement(node, ElementShape.DIAMOND));
+                else if("BLOCK_STATEMENT".equals(node.nodeName))
+                    nodeList.add(new GraphElement(node, ElementShape.SQUARE));
+            }*/
+        return nodeList;
     }
 
     private boolean isJumped(ASTEntry node){
