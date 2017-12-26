14025b2c1907c7de9c56fe7740264862c9f6cc69
Arseny Zorin <zorinarseny@yandex.ru>
2017-12-26 20:10:16 +0300

    Change jumped elems

--- Lab1/TreeBuilder/src/main/java/Main.java
+++ Lab1/TreeBuilder/src/main/java/Main.java
@@ -1,16 +1,6 @@
-/**
- * Created by sobol on 3/15/17.
- */
-
-//import py4j.GatewayServer;
-//import sun.rmi.server.ActivatableServerRef;
-
-//import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
-//import static py4j.GatewayServer.DEFAULT_PORT;
 
 public class Main {
     private final static PsiGen generator = new PsiGen();
@@ -20,10 +10,10 @@ public class Main {
             "BRACE", "SEMICOLON", "PARENTH", "_KEYWORD", "COMMA"
     );
     private final static List<String> jumped = Arrays.asList(
-            "GOTO_KEYWORD", "CONTINUE_STATEMENT", "METHOD_CALL_EXPRESSION",
+            "GOTO_KEYWORD", "METHOD_CALL_EXPRESSION",
             "WHILE_STATEMENT", "FOR_STATEMENT", "IF_STATEMENT",
             "FOREACH_STATEMENT", "SWITCH_STATEMENT", "DO_WHILE_STATEMENT",
-            "BREAK_STATEMENT", "SWITCH_LABEL_STATEMENT", "CASE_KEYWORD",
+            "SWITCH_LABEL_STATEMENT", "CASE_KEYWORD",
             "ELSE_KEYWORD", "BINARY_EXPRESSION"
     );
 
