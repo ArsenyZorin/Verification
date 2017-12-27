cc3fd44a44831659d146cbbb70ec57bb549a53f2
Arseny Zorin <zorinarseny@yandex.ru>
2017-12-27 12:01:06 +0300

    Test listing and picture

--- Lab1/report/listings/test.java
+++ Lab1/report/listings/test.java
@@ -1,15 +1,15 @@
 public class Main{
-	public void main(String[] args) {
+	public void main() {
 		int a = 0, b = 1, c = 0, i;
 		if(n < 2) return n;
 		for (i = 1; i < n; i++)
 		{
 			c = a + b;
-			if(a < b) return;
+			if(a < b) break;
 			int j = 1;
 			while (j < i) {
 				int k = b - a;
-				if(a < k) return;
+				if(a < k) continue;
 				int y = a + k; 
 				++j;
 			}
